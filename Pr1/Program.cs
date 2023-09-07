7f35eab69d4fdde00a5f382346f3e9a95d355ec3
BOTYA <95568178+botyak1234@users.noreply.github.com>
2023-09-07 20:46:51 +0400

    Add files via upload

---
+++ Pr1/Program.cs
@@ -0,0 +1,8 @@
+﻿string a = Console.ReadLine();
+Console.WriteLine("Как тебя зовут? {0}\n", a);
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Сколько тебе лет? {0}\n", b);
+int c = 2009-b;
+Console.WriteLine("{0}, ты родился в {1} году",a, c);
+
+Console.WriteLine("Press any key to continue");
