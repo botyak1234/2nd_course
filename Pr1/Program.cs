28b0996aabc38568d292c63144f4b220b28e0de4
BOTYA <95568178+botyak1234@users.noreply.github.com>
2023-09-07 20:36:39 +0400

    Add files via upload

---
+++ Pr1/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Номинал купюры = {0}\n", a);
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Количество купюр = {0}\n", b);
+double c = a*b;
+Console.WriteLine("Сумма денег = {0:N}", c);
+
+Console.WriteLine("Для продолжения нажмите любую клавишу...");
