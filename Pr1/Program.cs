48a6dc82b0f7af43d180f244630ee26e84449a28
BOTYA <95568178+botyak1234@users.noreply.github.com>
2023-09-07 20:40:20 +0400

    Add files via upload

---
+++ Pr1/Program.cs
@@ -0,0 +1,8 @@
+﻿double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите сумму вклада = {0}\n", a);
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите процент по вкладу = {0}\n", b);
+double c = a*b/100;
+Console.WriteLine("Через год начисляется = {0:N}р", c);
+
+Console.WriteLine("Для продолжения нажмите любую клавишу...");
