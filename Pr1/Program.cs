a89ec53f8d891a5a6540369d821b8b0c94129bd6
BOTYA <95568178+botyak1234@users.noreply.github.com>
2023-09-07 20:31:27 +0400

    Add files via upload

---
+++ Pr1/Program.cs
@@ -0,0 +1,9 @@
+﻿float a = float.Parse(Console.ReadLine());
+Console.WriteLine("a = {0}\n", a);
+float b = float.Parse(Console.ReadLine());
+Console.WriteLine("b = {0}\n", b);
+float c = float.Parse(Console.ReadLine());
+Console.WriteLine("c = {0}\n", c);
+
+Console.WriteLine("({0:F2}+{1:F2})+{2:F2}={0:F2}+({1:F2}+{2:F2})", a, b, c);
+Console.WriteLine("Для продолжения нажмите любую клавишу...");
