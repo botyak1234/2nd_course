261adb4b65de46282c8d73fac69d6c791b8c16b7
BOTYA <95568178+botyak1234@users.noreply.github.com>
2023-09-07 20:15:09 +0400

    Add files via upload

---
+++ Pr1/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = int.Parse(Console.ReadLine());
+Console.WriteLine("a = {0}\n", a);
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("b = {0}\n", b);
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine("c = {0}\n", b);
+int d = a + b + c;
+Console.WriteLine("{0} + {1} + {2} = {3}", a, b, c, d);
+Console.WriteLine("Для продолжения нажмите любую клавишу...");
