f6bc09d0de9510288978762e0217f3302e61e15f
BOTYA <95568178+botyak1234@users.noreply.github.com>
2023-09-08 11:47:09 +0300

    Add files via upload

---
+++ pr2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class cub{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int sum = b % 10 + b / 100 + b / 10 % 10;
+        string x = sum % a == 0? "да":"нет";
+        Console.WriteLine(x);
+    }
+}
