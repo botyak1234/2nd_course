0f1a46bc7b3409ef139fbdb0505dc33424206907
BOTYA <95568178+botyak1234@users.noreply.github.com>
2023-09-13 23:18:25 +0400

    Add files via upload

---
+++ pr3/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+class chisla
+{
+    static void Main()
+    {
+        Console.Write("A = ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("B = ");
+        int b = int.Parse(Console.ReadLine());
+        for (int i = a; i < b; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        Console.WriteLine("Press any key to continue");
+    }
+}
