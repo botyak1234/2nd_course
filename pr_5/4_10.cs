e5df13cb39d0386a0fc47d4a413f4b61d1b98340
BOTYA <95568178+botyak1234@users.noreply.github.com>
2023-09-21 22:04:20 +0400

    Create 4_10.cs

---
+++ pr_5/4_10.cs
@@ -0,0 +1,29 @@
+
+using System;
+using System.ComponentModel.Design;
+
+class chisla
+{
+    static void rec(int n)
+    {
+        if (n == 0) return;
+        draw(n);
+        rec(n - 1);
+    }
+
+    static void draw(int n)
+    {
+        for (int i = 0; i < n; i++) Console.Write('*');
+        Console.WriteLine();
+    }
+
+
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        rec(n);
+    }
+}
+
+
+    
