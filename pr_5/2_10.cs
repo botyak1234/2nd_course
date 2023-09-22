9986294a42ce3a8698905663fbb5fda85a16a560
BOTYA <95568178+botyak1234@users.noreply.github.com>
2023-09-22 12:48:13 +0400

    Update 2_10.cs

--- pr_5/2_10.cs
+++ pr_5/2_10.cs
@@ -1,10 +1,9 @@
 
 using System;
-using System.ComponentModel.Design;
 
 class chisla
 {
-    static double stepen (double x, int n)
+    static double stepen(double x, int n)
     {
         double power = 1;
         if (x == 0) power *= x;
@@ -24,40 +23,40 @@ class chisla
     static void Main()
     {
         Console.Write("a=");
-        int a = int.Parse(Console.ReadLine());
-        //Console.Write("b=");
-        //int b = int.Parse(Console.ReadLine());
-        //Console.Write("h=");
-        //int h = int.Parse(Console.ReadLine());
-        //int c = 1;
-        //int sum = 0;
-        //int st;
-        //for ( i = a; i <= b; i += h)
-        //{
-        //    st = stepen(i, c);
-        //    Console.Write(st);
-        //    sum += st;
-        //    c++;
-        //    Console.Write(' ');
-        //}
-        //Console.WriteLine();
-        //Console.WriteLine(sum);
+        double a = int.Parse(Console.ReadLine());
+        Console.Write("b=");
+        double b = int.Parse(Console.ReadLine());
+        Console.Write("h=");
+        int h = int.Parse(Console.ReadLine());
+        int c = 1;
+        double sum = 0;
+        double st;
+        for (double i = a; i <= b; i += h)
+        {
+            st = stepen(i, c);
+            Console.Write(st);
+            sum += st;
+            c++;
+            Console.Write(' ');
+        }
+        Console.WriteLine();
+        Console.WriteLine(sum);
 
 
 
 
-        //for (int x = a; x <= b; x++)
-        //{
-        //    for (int y = x + 1; y <= b; y++)
-        //    {
-        //        double z = (int)Math.Sqrt(stepen(x, 2) + stepen(y, 2));
-        //        if (z >= a && z <= b && stepen(z, 2) == stepen(x, 2) + stepen(y, 2)) Console.WriteLine("{0} {1} {2}", x, y, z); 
-        //    }
-        //}
+        for (double x = a; x <= b; x++)
+        {
+            for (double y = x + 1; y <= b; y++)
+            {
+                double z = (int)Math.Sqrt(stepen(x, 2) + stepen(y, 2));
+                if (z >= a && z <= b && stepen(z, 2) == stepen(x, 2) + stepen(y, 2)) Console.WriteLine("{0} {1} {2}", x, y, z);
+            }
+        }
 
         int n = 1;
         int two = 2;
-        while ( two <= a)
+        while (two <= a)
         {
             n++;
             two *= 2;
