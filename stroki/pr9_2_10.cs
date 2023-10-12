c536df0081a42ea9e539ee8d554523a8552ae3c6
BOTYA <95568178+botyak1234@users.noreply.github.com>
2023-10-12 22:18:26 +0400

    Create pr9_2_10.cs

---
+++ stroki/pr9_2_10.cs
@@ -0,0 +1,36 @@
+
+using System;
+using System.ComponentModel.Design;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.IO;
+
+class chisla
+{
+
+
+
+    static void Main()
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        using (StreamReader file_f = new StreamReader("E:\\C++\\help\\help\\f.txt", Encoding.GetEncoding(1251)))
+        {
+            using (StreamWriter file_h = new StreamWriter("E:\\C++\\help\\help\\h.txt"))
+            {
+                using (StreamWriter file_g = new StreamWriter("E:\\C++\\help\\help\\g.txt"))
+                {
+                    string sline;
+                    while ((sline = file_f.ReadLine()) != null)
+                    {
+                        foreach (char symb in sline)
+                        {
+                            if (Char.IsPunctuation(symb)) file_g.Write("{0} ", symb);
+                            else file_h.Write("{0}", symb);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
