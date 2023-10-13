a098cbc4b1c2b452f70d02225a6baf0685b57675
BOTYA <95568178+botyak1234@users.noreply.github.com>
2023-10-13 11:44:01 +0300

    Update pr8_3_10.cs

--- stroki/pr8_3_10.cs
+++ stroki/pr8_3_10.cs
@@ -1,18 +1,26 @@
 using System;
 using System.ComponentModel.Design;
+using System.Globalization;
+using System.Linq.Expressions;
 
 class chisla
 {
-   
+
 
 
     static void Main()
     {
         int count = 0;
         string s = Console.ReadLine();
-        foreach(char i in s)
+        List<string> strings_ = new List<string>(s.Split());
+        foreach (string slovo in strings_)
         {
-            if (Char.IsUpper(i)) count++;
+            bool flag = true;
+            foreach (char symbol in slovo)
+            {
+                if (Char.IsLower(symbol)) flag = false;
+            }
+            if (flag) ++count;
         }
         Console.WriteLine(count);
     }
