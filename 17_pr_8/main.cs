98521e8df3eba375a7022c562ffc325502079636
BOTYA <95568178+botyak1234@users.noreply.github.com>
2023-12-01 00:54:50 +0400

    Create main.cs

---
+++ 17_pr_8/main.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel.Design;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+class Programm
+{
+
+    static void Main()
+    {
+
+    }
+       
+}
