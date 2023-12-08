67f1eaaeddec6bfec12697e4f23a4cd8418bd113
BOTYA <95568178+botyak1234@users.noreply.github.com>
2023-12-08 13:37:58 +0400

    Update main.cs

--- 17_pr_8/main.cs
+++ 17_pr_8/main.cs
@@ -8,12 +8,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
 
-class Programm
+namespace ConsoleApp2
 {
-
-    static void Main()
+    class Programm
     {
 
+
+
+
+
+        static void Main()
+        {
+            using (StreamReader fileIn = new StreamReader("C:\\Users\\contest\\source\\repos\\ConsoleApp2\\ConsoleApp2\\input17.txt"))
+            {
+                using (StreamWriter fileOut = new StreamWriter("C:\\Users\\contest\\source\\repos\\ConsoleApp2\\ConsoleApp2\\output17.txt"))
+                {
+                    string s = fileIn.ReadLine();
+                    str line = new str(s);
+                    str line2 = new str(s);
+                    line.Show(fileOut);
+                    line.print_single(fileOut);
+                    line.print_max_copy(fileOut);
+                    fileOut.WriteLine(line.TotalCharacters);
+                    fileOut.WriteLine(line[3]);
+                    fileOut.WriteLine(!line);
+                    if (line)
+                    {
+                        fileOut.WriteLine(1);
+                    }
+                    else
+                    {
+                        fileOut.WriteLine(-1);
+                    }
+                    fileOut.WriteLine(line & line2);
+                    string stoka = line;
+                    str line3 = s;
+                }
+            }
+        }
     }
-       
 }
