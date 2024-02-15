4720fb67339b9192479289cc08b6bd32545a7af8
BOTYA <95568178+botyak1234@users.noreply.github.com>
2024-02-16 01:03:52 +0400

    Create Creditor.cs

---
+++ 18_19_9_/Creditor.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    [Serializable]
+    internal class Creditor : Client
+    {
+        
+        public double CreditAmount { get; }
+        public double Credit { get; }
+        public double RemainingCredit { get; }
+
+       
+        public Creditor(string lastName, DateTime startDate, double creditAmount, double credit, double remainingCredit) : base(lastName, startDate)
+        {
+            CreditAmount = creditAmount;
+            Credit = credit;
+            RemainingCredit = remainingCredit;
+        }
+
+      
+        public override void DisplayInfo(StreamWriter fileOut)
+        {
+            fileOut.WriteLine($"Creditor: {LastName}, Credit Amount: {CreditAmount}, Credit: {Credit}%, Remaining Credit: {RemainingCredit}");
+        }
+        
+    }
+}
