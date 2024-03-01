68d3543577073e0ee4b4c9b56483a52a00521909
BOTYA <95568178+botyak1234@users.noreply.github.com>
2024-03-01 12:02:56 +0300

    Update main.cs

--- 18_19_9_/main.cs
+++ 18_19_9_/main.cs
@@ -1,116 +1,152 @@
-using System;
-using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Collections.Generic;
+
 namespace ConsoleApp1
 {
-    
+
     class Program
     {
-        //BinaryFormatter formatter = new BinaryFormatter();
-
-        static Client[] ReadClientsFromFile(string fileIn)
+        static void Print(List<Client> clients)
         {
-            using (StreamReader In = new StreamReader(fileIn))
+            if (clients.Count == 0)
             {
-                try
+                Console.WriteLine("Список объектов пуст.");
+            }
+            else
+            {
+                Console.WriteLine("Список объектов:");
+                foreach (Client client in clients)
                 {
-                    var clientsList = new List<Client>();
-
-                    using (StreamReader reader = new StreamReader(fileIn))
-                    {
-                        string line;
-                        while ((line = reader.ReadLine()) != null)
-                        {
-
-                            string[] data = line.Split(',');
-
-
-                            switch (data[0])
-                            {
-                                case nameof(Depositor):
-                                    clientsList.Add(new Depositor(
-
-                                        data[1],
-                                        DateTime.Parse(data[2]),
-                                        double.Parse(data[3]),
-                                        double.Parse(data[4]))
-                                    );
-                                    break;
-                                case nameof(Creditor):
-                                    clientsList.Add(new Creditor
-                                    (
-                                        data[1],
-                                        DateTime.Parse(data[2]),
-                                        double.Parse(data[3]),
-                                        double.Parse(data[4]),
-                                        double.Parse(data[5])
-                                    ));
-                                    break;
-                                case nameof(Organization):
-                                    clientsList.Add(new Organization
-                                    (
-                                        data[1],
-                                        DateTime.Parse(data[2]),
-                                        data[3],
-                                        double.Parse(data[4])
-                                    ));
-                                    break;
-                                default:
-                                    Console.WriteLine($"Неизвестный клиент: {data[0]}");
-                                    break;
-                            }
-                        }
-                    }
-
-                    return clientsList.ToArray();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Не удалось прочитать файл: {ex.Message}");
-                    return null;
+                    Console.WriteLine(client.ToString());
                 }
             }
         }
 
 
-        static void Main(string[] args)
+        //    static List<Client> ReadClientsFromFile(string fileIn)
+        //{
+        //    using (StreamReader In = new StreamReader(fileIn))
+        //    {
+        //        try
+        //        {
+        //            var clientsList = new List<Client>();
+
+        //            using (StreamReader reader = new StreamReader(fileIn))
+        //            {
+        //                string line;
+        //                while ((line = reader.ReadLine()) != null)
+        //                {
+
+        //                    string[] data = line.Split(',');
+
+
+        //                    switch (data[0])
+        //                    {
+        //                        case nameof(Depositor):
+        //                            clientsList.Add(new Depositor(
+
+        //                                data[1],
+        //                                DateTime.Parse(data[2]),
+        //                                double.Parse(data[3]),
+        //                                double.Parse(data[4]))
+        //                            );
+        //                            break;
+        //                        case nameof(Creditor):
+        //                            clientsList.Add(new Creditor
+        //                            (
+        //                                data[1],
+        //                                DateTime.Parse(data[2]),
+        //                                double.Parse(data[3]),
+        //                                double.Parse(data[4]),
+        //                                double.Parse(data[5])
+        //                            ));
+        //                            break;
+        //                        case nameof(Organization):
+        //                            clientsList.Add(new Organization
+        //                            (
+        //                                data[1],
+        //                                DateTime.Parse(data[2]),
+        //                                data[3],
+        //                                double.Parse(data[4])
+        //                            ));
+        //                            break;
+        //                        default:
+
+        //                            break;
+        //                    }
+        //                }
+        //            }
+
+        //            return clientsList;
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Console.WriteLine($"Не удалось прочитать файл: {ex.Message}");
+        //            return null;
+        //        }
+        //    }
+        //}
+
+
+        static void Main()
         {
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            string fileIn = "C:\\Users\\Пользователь\\source\\repos\\help\\help\\help_in.txt";
+            string fileOut = "C:\\Users\\Пользователь\\source\\repos\\help\\help\\help_out.txt";
             
-            string fileIn = "C:\\Users\\Danil\\source\\repos\\ConsoleApp1\\ConsoleApp1\\help_in.txt";
-            string fileOut = "C:\\Users\\Danil\\source\\repos\\ConsoleApp1\\ConsoleApp1\\help_out.txt";
-            using (StreamWriter Out = new StreamWriter(fileOut))
-            {
-                Client[] clients = ReadClientsFromFile(fileIn);
-
-                if (clients != null)
+                List<Client> clients = new List<Client>{ };
+                
+               
+                
+                using (FileStream f = new FileStream("C:\\Users\\Пользователь\\source\\repos\\help\\help\\help_in.dat",
+                FileMode.OpenOrCreate))
                 {
-
-                    Out.WriteLine("Полная информация");
-                    foreach (Client client in clients)
+                    if (f.Length != 0)
                     {
-                        client.DisplayInfo(Out);
+                        clients = (List<Client>)formatter.Deserialize(f);
                     }
+                }
 
 
-                    DateTime targetDate = new DateTime(2023, 2, 1);
-                    Out.WriteLine($"Клиенты с {targetDate}:");
+                clients.Add(new Organization("HELP", DateTime.Parse("2023-01-10"), "0", 0));
+            clients.Add(new Creditor("Smith", DateTime.Parse("2023-01-5"), 0, 0, 0));
+                if (clients.Count== 0)
+                {
+                    Console.WriteLine("Список пуст");
+                }
+                else 
+                { 
+                    
+                    Console.WriteLine("Полная информация");
+                     Print(clients);
+
+
+                     DateTime targetDate = new DateTime(2023, 2, 1);
+                    Console.WriteLine($"Клиенты с {targetDate}:");
                     foreach (Client client in clients)
                     {
                         if (client.IsMatch(targetDate))
                         {
-                            client.DisplayInfo(Out);
+                            Console.WriteLine(client.ToString());
                         }
                     }
 
-                    Array.Sort(clients);
-                    Out.WriteLine("Отсортированная информация");
+                    clients.Sort();
+                    Console.WriteLine("Отсортированная информация");
                     foreach (Client client in clients)
                     {
-                        client.DisplayInfo(Out);
+                        Console.WriteLine(client.ToString());
+                    }
+                    using (FileStream f = new FileStream("C:\\Users\\Пользователь\\source\\repos\\help\\help\\help_in.dat",
+                FileMode.OpenOrCreate))
+                    {
+                        formatter.Serialize(f, clients);
                     }
                 }
-            }
         }
     }
 }
+
+
+
+
